72495a5d877a032508cd9fb1b30209380d32ddf7
icecat <icecat2012@gmail.com>
2016-12-06 17:33:36 +0800

    fix default all select

--- weather.py
+++ weather.py
@@ -7,7 +7,16 @@ import json
 
 Data_set = input("請輸入資料集編號: ")
 Location_name = input("請輸入地點(預設全選): ")
-target_url = urllib.request.Request('http://opendata.cwb.gov.tw/api/v1/rest/datastore/'+Data_set+'?locationName=' + urllib.parse.quote(Location_name, safe=''))
+
+#connect to cwb api
+urll = 'http://opendata.cwb.gov.tw/api/v1/rest/datastore/'+Data_set+'?'
+Location_name = Location_name.strip()
+if Location_name:
+	urll = urll+'locationName='
+	target_url = urllib.request.Request(urll+ urllib.parse.quote(Location_name, safe='')+'&sort=time')
+else:
+	target_url = urllib.request.Request(urll+ urllib.parse.quote(Location_name, safe='')+'sort=time')
+
 with open("token","r") as token_file:
     token = token_file.readline().rstrip('\n')
     print(token)
